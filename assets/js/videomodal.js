2b448d5949a2f57a00161f6d8cb51f0723de362d
djegoburgo <jeromburgaud@gmail.com>
2015-12-04 13:52:05 +0100

    import of the whole project

---
+++ assets/js/videomodal.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('#logistic-modal-4').on('shown.bs.modal', function (e) {
+        var src = $('#videowrapper').attr('data-iframe-src');
+        $('#videowrapper').attr('src', src);
+    });
+
+    $('#logistic-modal-4').on('hidden.bs.modal', function (e) {
+        $('#videowrapper').attr('src', '');
+    });
+});
+
+
+
+
+
+
+
+
+
