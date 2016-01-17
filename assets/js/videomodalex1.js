0c3323d3da9a4ad6c5a9f0ef17bc35c0f629b0c0
djegoburgo <jeromburgaud@gmail.com>
2016-01-17 13:09:10 +0100

    dernière MAJ fr

--- assets/js/videomodalex1.js
+++ assets/js/videomodalex1.js
@@ -1,10 +1,10 @@
 $(function () {
     $('#event-modal-2').on('shown.bs.modal', function (e) {
-        var src = $('#videowrapper4').attr('data-iframe-src');
-        $('#videowrapper4').attr('src', src);
+        var src = $('#videowrapper5').attr('data-iframe-src');
+        $('#videowrapper5').attr('src', src);
     });
 
     $('#event-modal-2').on('hidden.bs.modal', function (e) {
-        $('#videowrapper4').attr('src', '');
+        $('#videowrapper5').attr('src', '');
     });
 });
